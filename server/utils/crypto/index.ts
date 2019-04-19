d82f153292dc1a59f300f24d5fc91840c7e6e60a
Jozef Piecyk <jozollo@gmail.com>
2019-04-19 22:33:54 +0200

    correct typing in crypto

--- server/utils/crypto/index.ts
+++ server/utils/crypto/index.ts
@@ -2,9 +2,9 @@ import * as bcrypt from 'bcryptjs';
 const { HASH_SALT_ROUNDS } = process.env;
 
 export const hashPassword = (password): string => {
-  const salt = bcrypt.genSaltSync(HASH_SALT_ROUNDS);
+  const salt = bcrypt.genSaltSync(Number(HASH_SALT_ROUNDS));
   return bcrypt.hashSync(password, salt);
 };
 
-export const comparePassword = (candidatePassword: string, actualPassword: string): boolean =>
+export const comparePassword = (candidatePassword: string, actualPassword: string): Promise<boolean> =>
   bcrypt.compare(candidatePassword, actualPassword);
